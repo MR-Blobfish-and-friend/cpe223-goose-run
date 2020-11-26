b931508271c5bfd2ac94fc999235a82f9d41f540
Thanapat-Mah <74485784+Thanapat-Mah@users.noreply.github.com>
2020-11-26 14:34:31 +0700

    Add files via upload

---
+++ 26_1433_ScoreBug/sources_1/draw_bean.v
@@ -0,0 +1,97 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/15/2020 03:24:17 PM
+// Design Name: 
+// Module Name: draw_bean
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`include "draw_bean_graphic.v"
+//task: draw_one_floor_bean
+//task: draw_one_flying_bean
+
+module draw_bean(
+    input [9:0] x,
+    input [9:0] y,
+    input clk,
+    input check_hit,
+    input reset,
+    output bean,
+    output [11:0] bean_rgb
+    );
+     
+    reg isBean;
+    integer beans [0:2];
+    reg beans_type [0:2];
+    reg isB[0:2];
+    integer furthest, minVal, maxVal, ran;
+    integer height = 40;
+    integer width = 30;
+    integer pos_shift = 5;
+    integer i;
+    reg [11:0] rgb_reg;
+    reg hit_ctrl;
+    
+    initial begin 
+        beans[0] = 250;
+        beans[1] = 450;
+        beans[2] = 750;
+        beans_type[0] = 0;
+        beans_type[1] = 1;
+        beans_type[2] = 0;
+        furthest = 750;
+        minVal = 400;
+        maxVal = 700;
+        hit_ctrl = 0;
+    end
+    
+    //draw all bean
+    always @(*) begin
+        if (check_hit) hit_ctrl = 1;
+        if (reset) hit_ctrl = 0;
+        for (i = 0; i <= 2; i=i+1) begin
+            if (beans_type[i] == 0) draw_one_floor_bean(x, y, beans[i], rgb_reg, isB[i]);
+            else if (beans_type[i] == 1) draw_one_flying_bean(x, y, beans[i], rgb_reg, isB[i]);
+        end
+        isBean = isB[0] || isB[1] || isB[2];
+    end
+    
+    //position shift
+    always @(posedge clk)
+    begin
+        if (reset) begin
+            beans[0] = 250;
+            beans[1] = 450;
+            beans[2] = 750;
+        end
+        if (~hit_ctrl) begin
+            //furthest = furthest - pos_shift;
+            for(i = 0; i < 3; i=i+1) begin
+                beans[i] = beans[i] - pos_shift;
+                if (beans[i] <= 0) begin
+                    beans[i] = 700;
+    //                ran = $urandom_range(400, 700);
+    //                beans[i] = furthest + ran;
+    //                furthest = furthest + ran;
+                end
+            end
+        end
+    end
+    
+    assign bean = isBean;
+    assign bean_rgb = rgb_reg;
+    
+endmodule
---
+++ 26_1433_ScoreBug/sources_1/draw_goose.v
@@ -0,0 +1,122 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/15/2020 02:42:04 PM
+// Design Name: 
+// Module Name: draw goose
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`include"draw_goose_graphic.v"
+//include these task:
+//  draw_goose_run
+//  draw_goose_slide
+//  draw_goose_jump
+
+module draw_goose(
+    input [9:0] x,
+    input [9:0] y,
+    input clk,
+    input btnL,
+    input btnR,
+    input check_hit,
+    input reset,
+    output goose,
+    output [11:0] goose_rgb
+    );
+    
+    reg isGoose;
+    integer posy = 380;
+    reg isJump = 0;
+    reg isSlide = 0;
+    integer jumpCount = 0;
+    integer jumpList [0:30];
+    reg [11:0] rgb_reg;
+    reg hit_ctrl;
+    
+    //initiate jumpList
+    initial
+    begin
+        jumpList[0] = 15;
+        jumpList[1] = 12;
+        jumpList[2] = 9;
+        jumpList[3] = 7;
+        jumpList[4] = 5;
+        jumpList[5] = 4;
+        jumpList[6] = 3;
+        jumpList[7] = 3;
+        jumpList[8] = 2;
+        jumpList[9] = 2;
+        jumpList[10] = 2;
+        jumpList[11] = 1;
+        jumpList[12] = 1;
+        jumpList[13] = 1;
+        jumpList[14] = 1;
+        jumpList[15] = 0;
+        jumpList[16] = -1;
+        jumpList[17] = -1;
+        jumpList[18] = -1;
+        jumpList[19] = -1;
+        jumpList[20] = -2;
+        jumpList[21] = -2;
+        jumpList[22] = -2;
+        jumpList[23] = -3;
+        jumpList[24] = -3;
+        jumpList[25] = -4;
+        jumpList[26] = -5;
+        jumpList[27] = -7;
+        jumpList[28] = -9;
+        jumpList[29] = -12;
+        jumpList[30] = -15;
+        hit_ctrl = 0;
+    end
+    
+    //control goose mode
+    always @(*) begin
+        if (check_hit) hit_ctrl = 1;
+        if (reset) hit_ctrl = 0;
+        if (isJump) draw_goose_jump(x, y, posy, isGoose, rgb_reg);
+        else if (isSlide) draw_goose_slide(x, y, isGoose, rgb_reg);
+        else draw_goose_run(x, y, isGoose, rgb_reg);
+    end
+     
+    //shift position of jumping goose
+    always @(posedge clk)
+    begin
+        if (~hit_ctrl) begin
+            if (btnL) isJump = 1;
+            else if (btnR) isSlide = 1;
+            else if (~btnR) isSlide = 0;      
+        end
+        if (reset) begin
+            isJump = 0;
+            isSlide = 0;
+            jumpCount = 0;
+            posy = 380;
+        end          
+        if (jumpCount == 31) begin
+                jumpCount = 0;
+                isJump = 0;
+        end 
+        if (isJump) begin
+                posy = posy - jumpList[jumpCount];
+                jumpCount = jumpCount + 1;
+        end
+    end
+
+    assign goose = isGoose;
+    assign goose_rgb = rgb_reg;
+    
+endmodule
