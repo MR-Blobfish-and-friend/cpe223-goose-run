b9ee26d2d56614ba8e7fb352cc5b20a7bad70851
THSpp <74483748+THSpp@users.noreply.github.com>
2020-12-05 16:04:39 +0700

    Update draw_ground.v

--- draw_ground.v
+++ draw_ground.v
@@ -35,7 +35,7 @@ task draw_ground1(
             end
         end
         else if (398 <= y &&y <= 401) begin
-            if (g-39 <= x && x <= g-34) begin
+            if ((g-39 <= x && x <= g-34) || (g-5 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
@@ -43,23 +43,15 @@ task draw_ground1(
                     rgb_reg = 12'h0a0;
                     isGround = 1;
             end
-            else if (g-5 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
-            end
         end
         else if (402 <= y &&y <= 404) begin
-            if (g-39 <= x && x <= g-28) begin
+            if ((g-39 <= x && x <= g-28) || (g-11 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
             else if (g-27 <= x && x <= g-12) begin
                     rgb_reg = 12'h0a0;
                     isGround = 1;
-            end
-            else if (g-11 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
             end
         end
         else if (405 <= y && y <= 406) begin
@@ -69,7 +61,7 @@ task draw_ground1(
             end
         end
         else if (407 <= y && y <= 409) begin
-            if (g-39 <= x && x <= g-14) begin
+            if ((g-39 <= x && x <= g-14) || (g-10 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
@@ -77,13 +69,9 @@ task draw_ground1(
                 rgb_reg = 12'hda6;
                 isGround = 1;
             end
-            else if (g-10 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
-            end
         end
         else if (410 <= y && y <= 412) begin
-            if (g-39 <= x && x <= g-30) begin
+            if ((g-39 <= x && x <= g-30) || (g-24 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
@@ -91,38 +79,26 @@ task draw_ground1(
                 rgb_reg = 12'hda6;
                 isGround = 1;
             end
-            else if (g-24 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
-            end
         end
         else if (413 <= y && y <= 414) begin
-            if (g-39 <= x && x <= g-30) begin
+            if ((g-39 <= x && x <= g-30) || (g-21 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
             else if (g-29 <= x && x <= g-22) begin
                 rgb_reg = 12'hda6;
                 isGround = 1;
-            end
-            else if (g-21 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
-            end             
+            end           
         end
         else if (415 <= y && y <= 419) begin
-            if (g-39 <= x && x <= g-27) begin
+            if ((g-39 <= x && x <= g-27) || (g-21 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
             else if (g-26 <= x && x <= g-22) begin
                 rgb_reg = 12'hda6;
                 isGround = 1;
-            end
-            else if (g-21 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
-            end             
+            end           
         end
         else if (420 <= y && y <= 480) begin
             if (g-39 <= x && x <= g) begin
@@ -155,7 +131,7 @@ task draw_ground2(
             end
         end
         else if ((y == 408) || (y == 412)) begin
-            if (g-39 <= x && x <= g-21) begin
+            if ((g-39 <= x && x <= g-21) || (g-19 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
@@ -163,13 +139,9 @@ task draw_ground2(
                 rgb_reg = 12'hda6;
                 isGround = 1;
             end
-            else if (g-19 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
-            end
         end
         else if ((y == 409) || (y == 411)) begin
-            if (g-39 <= x && x <= g-22) begin
+            if ((g-39 <= x && x <= g-22) || (g-18 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
@@ -177,22 +149,14 @@ task draw_ground2(
                 rgb_reg = 12'hda6;
                 isGround = 1;
             end
-            else if (g-18 <= x && x <= g) begin
-                rgb_reg = 12'h742;
-                isGround = 1;
-            end
         end
         else if (y == 410) begin
-            if (g-39 <= x && x <= g-23) begin
+            if ((g-39 <= x && x <= g-23) || (g-17 <= x && x <= g)) begin
                 rgb_reg = 12'h742;
                 isGround = 1;
             end
             else if (g-22 <= x && x <= g-18) begin
                 rgb_reg = 12'hda6;
-                isGround = 1;
-            end
-            else if (g-17 <= x && x <= g) begin
-                rgb_reg = 12'h742;
                 isGround = 1;
             end
         end
